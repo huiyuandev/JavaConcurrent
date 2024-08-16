02a4b8dea5ea7fa08dfc89d6e03debe80d32f822
hy <2864798982@qq.com>
2024-08-16 11:11:55 +0800

    线程状态及统筹

--- src/main/java/base/ThreadState.java
+++ src/main/java/base/ThreadState.java
@@ -16,9 +16,75 @@ public class ThreadState {
             }
         };
 
-        System.out.println(t1.getState());
-        t1.start();
-        System.out.println(t1.getState());
+        Thread t2 = new Thread("t2") {
+            @Override
+            public void run() {
+                while (true) { // runnable
+
+                }
+            }
+        };
+        t2.start();
+
+        Thread t3 = new Thread("t3") {
+            @Override
+            public void run() {
+                log.debug("running...");
+            }
+        };
+        t3.start();
+
+        Thread t4 = new Thread("t4") {
+            @Override
+            public void run() {
+                synchronized (ThreadState.class) {
+                    try {
+                        Thread.sleep(1000000); // timed_waiting
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        t4.start();
+
+        Thread t5 = new Thread("t5") {
+            @Override
+            public void run() {
+                try {
+                    t2.join(); // waiting
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        t5.start();
+
+        Thread t6 = new Thread("t6") {
+            @Override
+            public void run() {
+                synchronized (ThreadState.class) { // blocked
+                    try {
+                        Thread.sleep(1000000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        t6.start();
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.debug("t1 state {}", t1.getState());
+        log.debug("t2 state {}", t2.getState());
+        log.debug("t3 state {}", t3.getState());
+        log.debug("t4 state {}", t4.getState());
+        log.debug("t5 state {}", t5.getState());
+        log.debug("t6 state {}", t6.getState());
     }
 
 }
