44b001a05e190eba76fccbabb3c4b6239f095725
hy <2864798982@qq.com>
2024-09-06 17:23:24 +0800

    线程活跃性：多把锁、死锁、活锁、饥饿

---
+++ src/main/java/safe/TestMultiLock.java
@@ -0,0 +1,40 @@
+package safe;
+
+import lombok.extern.slf4j.Slf4j;
+import util.Sleeper;
+
+
+public class TestMultiLock {
+    public static void main(String[] args) {
+        BigRoom bigRoom = new BigRoom();
+        new Thread(() -> {
+            bigRoom.study();
+        },"小南").start();
+        new Thread(() -> {
+            bigRoom.sleep();
+        },"小女").start();
+    }
+}
+
+@Slf4j(topic = "c.BigRoom")
+class BigRoom {
+
+    private final Object studyRoom = new Object();
+
+    private final Object bedRoom = new Object();
+
+    public void sleep() {
+        synchronized (bedRoom) {
+            log.debug("sleeping 2 小时");
+            Sleeper.sleep(2);
+        }
+    }
+
+    public void study() {
+        synchronized (studyRoom) {
+            log.debug("study 1 小时");
+            Sleeper.sleep(1);
+        }
+    }
+
+}
